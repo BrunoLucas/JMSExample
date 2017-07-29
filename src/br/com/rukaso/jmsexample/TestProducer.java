1495e30a25379eba213fd39859c1a3b9d252038d
Lucas <rukaso.mero@gmail.com>
2017-07-29 15:23:27 -0300

    commit producers and consumers

---
+++ src/br/com/rukaso/jmsexample/TestProducer.java
@@ -0,0 +1,42 @@
+package br.com.rukaso.jmsexample;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TestProducer {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+
+		InitialContext context = new InitialContext();
+
+		ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination fila = (Destination) context.lookup("financeiro");
+
+		MessageProducer producer = session.createProducer(fila);
+		
+		for(int i = 0; i < 2000; i++){
+			Message mensagem = session.createTextMessage("<pedido><id> "+ i + "</id></pedido>");
+			producer.send(mensagem);
+		}
+		
+		connection.close();
+		context.close();
+
+	}
+}
---
+++ src/br/com/rukaso/jmsexample/TesteComsumidor.java
@@ -0,0 +1,54 @@
+package br.com.rukaso.jmsexample;
+
+import java.util.Scanner;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+public class TesteComsumidor {
+
+	public static void main(String[] args) throws NamingException, JMSException {
+		
+		InitialContext context = new InitialContext();
+		
+		ConnectionFactory connectionFactory = (ConnectionFactory) context.lookup("ConnectionFactory");
+		Connection connection = connectionFactory.createConnection();
+		connection.start();
+		
+		Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		Destination fila = (Destination) context.lookup("financeiro");
+		
+		MessageConsumer consumer = session.createConsumer(fila);
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message mensagem) {
+
+				TextMessage textMessage  = (TextMessage)mensagem;
+		        try {
+					System.out.println(textMessage.getText());
+				} catch (JMSException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}							
+			}
+		});
+		
+		
+		new Scanner(System.in).nextLine();
+		
+		connection.close();
+		context.close();
+		
+		
+	}
+}
